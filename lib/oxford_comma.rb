cd4b6355992cbd9edb23554544fbd31a5367e4d0
Julian Patterson <julian.patterson@gmail.com>
2021-02-07 14:18:24 -0500

    commit tree 78bddcdc699e228171cc4f86e5dac5fa4ab21f1d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+    if array.length < 2
+        array.join
+    elsif array.length == 2
+        array.insert(1, " and ").join
+    elsif array.length >= 3
+        array[-1] = "and #{array[-1]}" 
+        array.join(", ")
+
+
+    end
+
 
 end
